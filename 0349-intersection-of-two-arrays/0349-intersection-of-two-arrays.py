843caadf0199e42096e8582333d25ec8aa2bfc52
Pooja Khatri <poojakhatri888@gmail.com>
2025-05-29 20:00:59 +0530

    Time: 23 ms (11.09%), Space: 12.6 MB (57.49%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -7,9 +7,9 @@ class Solution(object):
         """
         result = []
         for ele in nums1:
-            if ele in nums2:
+            if ele in nums2 and ele not in result:
                 result.append(ele)
-        return list(set(result))
+        return result
 
 
         
