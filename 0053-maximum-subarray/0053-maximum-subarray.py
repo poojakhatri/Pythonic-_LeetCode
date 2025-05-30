939ca534b3f10d4a7aa3a0acf683f1ef2cdc1de1
Pooja Khatri <poojakhatri888@gmail.com>
2025-05-30 23:12:37 +0530

    Time: 61 ms (67%), Space: 21.9 MB (38.31%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = 0
+        max_ = nums[0]
+
+        for num in nums:
+            if total < 0:
+                total = 0
+            total += num
+            max_ =  max(max_, total)
+        return max_
