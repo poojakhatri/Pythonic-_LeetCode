f6a1bb3a91cc831114c5e74bf5ec9b356266d7ca
Pooja Khatri <poojakhatri888@gmail.com>
2025-06-03 19:21:07 +0530

    Time: 37 ms (66.29%), Space: 14.6 MB (91.46%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        prefix_sum = 0
+        sum_map = {0:1}
+
+        for num in nums:
+            prefix_sum += num
+            if prefix_sum - k in sum_map:
+                count += sum_map[prefix_sum - k]
+            sum_map[prefix_sum] = sum_map.get(prefix_sum, 0) + 1
+        return count 
