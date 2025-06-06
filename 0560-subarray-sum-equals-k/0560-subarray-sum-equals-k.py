94b53161b064697ca32b1e7252e2268c5594c31f
Pooja Khatri <poojakhatri888@gmail.com>
2025-06-06 10:13:49 +0530

    Time: 36 ms (68.47%), Space: 14.7 MB (91.77%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -7,11 +7,10 @@ class Solution(object):
         """
         count = 0
         prefix_sum = 0
-        sum_map = {0:1}
-
+        sum_map = {0: 1}
         for num in nums:
             prefix_sum += num
-            if prefix_sum - k in sum_map:
-                count += sum_map[prefix_sum - k]
+            if prefix_sum - k  in sum_map:
+                count += sum_map[prefix_sum-k]
             sum_map[prefix_sum] = sum_map.get(prefix_sum, 0) + 1
         return count 
