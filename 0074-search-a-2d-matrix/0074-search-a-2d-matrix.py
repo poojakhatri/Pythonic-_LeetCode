bec5fc7e3a64a2c78d0c4d8e7cc877cfa60670cd
Pooja Khatri <poojakhatri888@gmail.com>
2025-06-08 21:22:04 +0530

    Time: 0 ms (100%), Space: 12.7 MB (60.14%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            for val in row:
+                if val == target:
+                    return True
+        return False
+        
