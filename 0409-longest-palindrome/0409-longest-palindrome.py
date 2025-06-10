a5be6b16769dab26bc28a0688f6d7b334bdfa2ea
Pooja Khatri <poojakhatri888@gmail.com>
2025-06-10 08:55:58 +0530

    Time: 3 ms (73.9%), Space: 12.5 MB (18.16%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = {}
+        r = 0
+        for c in s:
+            d[c] = d.get(c, 0) + 1
+        for v in d.values():
+            r += v if v % 2 == 0 else v - 1
+        return r + 1 if r < len(s) else r
+        
