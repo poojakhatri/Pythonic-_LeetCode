40a5c60c38199b1ecbd0a5806959f06c1bd31feb
Pooja Khatri <poojakhatri888@gmail.com>
2025-08-28 10:31:13 +0530

    Time: 0 ms (100%), Space: 12.5 MB (44.8%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        word_list = s.split()
+        return len(word_list[-1])
+        
